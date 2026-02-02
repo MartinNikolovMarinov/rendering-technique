bc3ff52fc45d4bae0e5f88ddf4aa8fda0d15e299
MartinNikolovMarinov <martin_n_marinov@protonmail.com>
2026-02-02 18:12:50 +0200

    macro system to simplify snapshot test creation

--- tests/test_utils.h
+++ tests/test_utils.h
@@ -10,178 +10,61 @@ void compareFilesBytewise(const char* fileA, const char* fileB);
 constexpr const char* SNAPSHOT_ROOT_DIRECTORY = TEST_ASSETS_DIRECTORY "/snapshots";
 constexpr const char* TEST_OUTPUT_DIRECTORY = TEST_ASSETS_DIRECTORY "/test_output_directory";
 
-constexpr bool updateAllSnaps = false;
+constexpr bool updateAllSnapshots = false;
+
+// Snapshot cases define per-input parameters (name, file, dimensions, update flag, format, origin).
+#define SNAPSHOT_CASES(X) \
+    X(SIMPLE_TRIANGLE_BGRA8888, "01 Simple Triangle Scene", "01_simple_triangle.obj", 800, 800, updateAllSnapshots, BGRA8888, BottomLeft) \
+    X(SIMPLE_TRIANGLE_BGRX8888, "01 Simple Triangle Scene", "01_simple_triangle.obj", 800, 800, updateAllSnapshots, BGRX8888, BottomLeft) \
+    X(SIMPLE_TRIANGLE_BGRA5551, "01 Simple Triangle Scene", "01_simple_triangle.obj", 800, 800, updateAllSnapshots, BGRA5551, BottomLeft) \
+    X(SIMPLE_TRIANGLE_BGR555,   "01 Simple Triangle Scene", "01_simple_triangle.obj", 800, 800, updateAllSnapshots, BGR555,   BottomLeft) \
+    X(SIMPLE_TRIANGLE_BGR888,   "01 Simple Triangle Scene", "01_simple_triangle.obj", 800, 800, updateAllSnapshots, BGR888,   BottomLeft) \
+    X(FOUR_TRIANGLES_BGRA8888,  "02 Four Triangles Scene",  "02_triangles.obj",       800, 800, updateAllSnapshots, BGRA8888, BottomLeft) \
+    X(FOUR_TRIANGLES_BGRX8888,  "02 Four Triangles Scene",  "02_triangles.obj",       800, 800, updateAllSnapshots, BGRX8888, BottomLeft) \
+    X(FOUR_TRIANGLES_BGRA5551,  "02 Four Triangles Scene",  "02_triangles.obj",       800, 800, updateAllSnapshots, BGRA5551, BottomLeft) \
+    X(FOUR_TRIANGLES_BGR555,    "02 Four Triangles Scene",  "02_triangles.obj",       800, 800, updateAllSnapshots, BGR555,   BottomLeft) \
+    X(FOUR_TRIANGLES_BGR888,    "02 Four Triangles Scene",  "02_triangles.obj",       800, 800, updateAllSnapshots, BGR888,   BottomLeft)
+
+enum SnapshotIndex {
+#define SNAPSHOT_ENUM_ENTRY(id, case_name, file, w, h, upd, fmt, case_origin) SNAPSHOT_##id,
+#define X(id, case_name, file, w, h, upd, fmt, case_origin) SNAPSHOT_ENUM_ENTRY(id, case_name, file, w, h, upd, fmt, case_origin)
+    SNAPSHOT_CASES(X)
+#undef X
+#undef SNAPSHOT_ENUM_ENTRY
+    SNAPSHOT_COUNT
+};
+
+#define SNAPSHOT_INFO_ENTRY(id, case_name, file, w, h, upd, fmt, case_origin) \
+    { \
+        .wavefrontInputFileFullPath = TEST_ASSETS_DIRECTORY "/snapshot_tests_input_files/" file, \
+        .snapshotDirectory = SNAPSHOT_ROOT_DIRECTORY, \
+        .outputDirectory = TEST_OUTPUT_DIRECTORY, \
+        .updateSnapshots = upd, \
+        .width = w, \
+        .height = h, \
+        .pixelFormat = PixelFormat::fmt, \
+        .origin = Origin::case_origin, \
+    },
 
 inline TestSnapshotInfo testSnapshotInfos[] = {
-    //==================================================================================================================
-    // 01_simple_triangle.obj
-    //==================================================================================================================
-
-    {
-        .wavefrontInputFileFullPath = TEST_ASSETS_DIRECTORY "/snapshot_tests_input_files/01_simple_triangle.obj",
-        .snapshotDirectory = SNAPSHOT_ROOT_DIRECTORY,
-        .outputDirectory = TEST_OUTPUT_DIRECTORY,
-        .updateSnapshots = updateAllSnaps,
-        .width = 800,
-        .height = 800,
-        .pixelFormat = PixelFormat::BGRA8888,
-        .origin = Origin::BottomLeft,
-    },
-    {
-        .wavefrontInputFileFullPath = TEST_ASSETS_DIRECTORY "/snapshot_tests_input_files/01_simple_triangle.obj",
-        .snapshotDirectory = SNAPSHOT_ROOT_DIRECTORY,
-        .outputDirectory = TEST_OUTPUT_DIRECTORY,
-        .updateSnapshots = updateAllSnaps,
-        .width = 800,
-        .height = 800,
-        .pixelFormat = PixelFormat::BGRX8888,
-        .origin = Origin::BottomLeft,
-    },
-    {
-        .wavefrontInputFileFullPath = TEST_ASSETS_DIRECTORY "/snapshot_tests_input_files/01_simple_triangle.obj",
-        .snapshotDirectory = SNAPSHOT_ROOT_DIRECTORY,
-        .outputDirectory = TEST_OUTPUT_DIRECTORY,
-        .updateSnapshots = updateAllSnaps,
-        .width = 800,
-        .height = 800,
-        .pixelFormat = PixelFormat::BGRA5551,
-        .origin = Origin::BottomLeft,
-    },
-    {
-        .wavefrontInputFileFullPath = TEST_ASSETS_DIRECTORY "/snapshot_tests_input_files/01_simple_triangle.obj",
-        .snapshotDirectory = SNAPSHOT_ROOT_DIRECTORY,
-        .outputDirectory = TEST_OUTPUT_DIRECTORY,
-        .updateSnapshots = updateAllSnaps,
-        .width = 800,
-        .height = 800,
-        .pixelFormat = PixelFormat::BGR555,
-        .origin = Origin::BottomLeft,
-    },
-    {
-        .wavefrontInputFileFullPath = TEST_ASSETS_DIRECTORY "/snapshot_tests_input_files/01_simple_triangle.obj",
-        .snapshotDirectory = SNAPSHOT_ROOT_DIRECTORY,
-        .outputDirectory = TEST_OUTPUT_DIRECTORY,
-        .updateSnapshots = updateAllSnaps,
-        .width = 800,
-        .height = 800,
-        .pixelFormat = PixelFormat::BGR888,
-        .origin = Origin::BottomLeft,
-    },
-
-    //==================================================================================================================
-    // 02_triangles.obj
-    //==================================================================================================================
-
-    {
-        .wavefrontInputFileFullPath = TEST_ASSETS_DIRECTORY "/snapshot_tests_input_files/02_triangles.obj",
-        .snapshotDirectory = SNAPSHOT_ROOT_DIRECTORY,
-        .outputDirectory = TEST_OUTPUT_DIRECTORY,
-        .updateSnapshots = updateAllSnaps,
-        .width = 800,
-        .height = 800,
-        .pixelFormat = PixelFormat::BGRA8888,
-        .origin = Origin::BottomLeft,
-    },
-    {
-        .wavefrontInputFileFullPath = TEST_ASSETS_DIRECTORY "/snapshot_tests_input_files/02_triangles.obj",
-        .snapshotDirectory = SNAPSHOT_ROOT_DIRECTORY,
-        .outputDirectory = TEST_OUTPUT_DIRECTORY,
-        .updateSnapshots = updateAllSnaps,
-        .width = 800,
-        .height = 800,
-        .pixelFormat = PixelFormat::BGRX8888,
-        .origin = Origin::BottomLeft,
-    },
-    {
-        .wavefrontInputFileFullPath = TEST_ASSETS_DIRECTORY "/snapshot_tests_input_files/02_triangles.obj",
-        .snapshotDirectory = SNAPSHOT_ROOT_DIRECTORY,
-        .outputDirectory = TEST_OUTPUT_DIRECTORY,
-        .updateSnapshots = updateAllSnaps,
-        .width = 800,
-        .height = 800,
-        .pixelFormat = PixelFormat::BGRA5551,
-        .origin = Origin::BottomLeft,
-    },
-    {
-        .wavefrontInputFileFullPath = TEST_ASSETS_DIRECTORY "/snapshot_tests_input_files/02_triangles.obj",
-        .snapshotDirectory = SNAPSHOT_ROOT_DIRECTORY,
-        .outputDirectory = TEST_OUTPUT_DIRECTORY,
-        .updateSnapshots = updateAllSnaps,
-        .width = 800,
-        .height = 800,
-        .pixelFormat = PixelFormat::BGR555,
-        .origin = Origin::BottomLeft,
-    },
-    {
-        .wavefrontInputFileFullPath = TEST_ASSETS_DIRECTORY "/snapshot_tests_input_files/02_triangles.obj",
-        .snapshotDirectory = SNAPSHOT_ROOT_DIRECTORY,
-        .outputDirectory = TEST_OUTPUT_DIRECTORY,
-        .updateSnapshots = updateAllSnaps,
-        .width = 800,
-        .height = 800,
-        .pixelFormat = PixelFormat::BGR888,
-        .origin = Origin::BottomLeft,
-    },
+#define X(id, case_name, file, w, h, upd, fmt, case_origin) SNAPSHOT_INFO_ENTRY(id, case_name, file, w, h, upd, fmt, case_origin)
+    SNAPSHOT_CASES(X)
+#undef X
 };
+
+#define SNAPSHOT_TEST_ENTRY(id, case_name, file, w, h, upd, fmt, case_origin) \
+    { \
+        .name = case_name, \
+        .testFunction = runDirectRasterizationSnapshotTest, \
+        .userData = &testSnapshotInfos[SNAPSHOT_##id], \
+    },
 
 inline TestCreateInfo snapshotTests[] = {
-    //==================================================================================================================
-    // 01_simple_triangle.obj
-    //==================================================================================================================
-
-    {
-        .name = "01 Simple Triangle Scene",
-        .testFunction = runDirectRasterizationSnapshotTest,
-        .userData = &testSnapshotInfos[0]
-    },
-    {
-        .name = "01 Simple Triangle Scene",
-        .testFunction = runDirectRasterizationSnapshotTest,
-        .userData = &testSnapshotInfos[1]
-    },
-    {
-        .name = "01 Simple Triangle Scene",
-        .testFunction = runDirectRasterizationSnapshotTest,
-        .userData = &testSnapshotInfos[2]
-    },
-    {
-        .name = "01 Simple Triangle Scene",
-        .testFunction = runDirectRasterizationSnapshotTest,
-        .userData = &testSnapshotInfos[3]
-    },
-    {
-        .name = "01 Simple Triangle Scene",
-        .testFunction = runDirectRasterizationSnapshotTest,
-        .userData = &testSnapshotInfos[4]
-    },
-
-    //==================================================================================================================
-    // 02_triangles.obj
-    //==================================================================================================================
-
-    {
-        .name = "02 Four Triangles Scene",
-        .testFunction = runDirectRasterizationSnapshotTest,
-        .userData = &testSnapshotInfos[5]
-    },
-    {
-        .name = "02 Four Triangles Scene",
-        .testFunction = runDirectRasterizationSnapshotTest,
-        .userData = &testSnapshotInfos[6]
-    },
-    {
-        .name = "02 Four Triangles Scene",
-        .testFunction = runDirectRasterizationSnapshotTest,
-        .userData = &testSnapshotInfos[7]
-    },
-    {
-        .name = "02 Four Triangles Scene",
-        .testFunction = runDirectRasterizationSnapshotTest,
-        .userData = &testSnapshotInfos[8]
-    },
-    {
-        .name = "02 Four Triangles Scene",
-        .testFunction = runDirectRasterizationSnapshotTest,
-        .userData = &testSnapshotInfos[9]
-    },
+#define X(id, case_name, file, w, h, upd, fmt, case_origin) SNAPSHOT_TEST_ENTRY(id, case_name, file, w, h, upd, fmt, case_origin)
+    SNAPSHOT_CASES(X)
+#undef X
 };
+
+#undef SNAPSHOT_TEST_ENTRY
+#undef SNAPSHOT_INFO_ENTRY
+#undef SNAPSHOT_CASES
